06d0c607f42b8de349c41a1a1af6bc11057f76c6
Tagir Almasov <tccup1@gmail.com>
2021-05-02 14:36:09 +0000

    setting up telegraf

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,30 @@
 import * as functions from "firebase-functions";
+import { Telegraf } from 'telegraf'
 
+const BOT_TOKEN = process.env.token || functions.config().tg.token
+
+const bot = new Telegraf(BOT_TOKEN)
+
+bot.command('hello', (ctx) => ctx.reply('Hello, friend!'))
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!111d");
+export const hello = functions.https.onRequest(async (request, response) => {
+  if (request.query.token && request.query.token === BOT_TOKEN) {
+    try {
+      await bot.handleUpdate(request.body)
+    } catch (error) {
+      functions.logger.error("Error while processing", { structuredData: true });
+    } finally {
+      response.status(200).end()
+    }
+  } else {
+    functions.logger.warn("No auth", { structuredData: true });
+    response.status(403).send({
+      message: 'You lack auth'
+    })
+  }
+
 });
 
 

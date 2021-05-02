abec4b74c1e4fa3ed95e96239fb7b70d48b6ce79
Tagir Almasov <tccup1@gmail.com>
2021-05-02 15:05:21 +0000

    setup prettier and eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
-import * as functions from "firebase-functions";
-import { Telegraf } from 'telegraf'
+import * as functions from "firebase-functions"
+import { Telegraf } from "telegraf"
 
 const BOT_TOKEN = process.env.token || functions.config().tg.token
 
 const bot = new Telegraf(BOT_TOKEN)
 
-bot.command('hello', (ctx) => ctx.reply('Hello, friend!'))
+bot.command("hello", (ctx) => ctx.reply("Hello, friend!"))
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -14,17 +14,14 @@ export const hello = functions.https.onRequest(async (request, response) => {
     try {
       await bot.handleUpdate(request.body)
     } catch (error) {
-      functions.logger.error("Error while processing", { structuredData: true });
+      functions.logger.error("Error while processing", { structuredData: true })
     } finally {
       response.status(200).end()
     }
   } else {
-    functions.logger.warn("No auth", { structuredData: true });
+    functions.logger.warn("No auth", { structuredData: true })
     response.status(403).send({
-      message: 'You lack auth'
+      message: "You lack auth",
     })
   }
-
-});
-
-
+})

8273545b1a34d9140367fa4c87d0cb427e2ec118
Tagir Almasov <tccup1@gmail.com>
2021-05-05 22:07:20 +0000

    add spell checking

--- functions/src/index.spec.ts
+++ functions/src/index.spec.ts
@@ -12,14 +12,14 @@ import * as functions from "./index"
 
 describe("hello", () => {
   test("function should fail without auth", async () => {
-    const req: any = { query: { text: "input" } } // until I figure out how to mock a functinon proper
+    const req: any = { query: { text: "input" } } // until I figure out how to mock a function proper
     const res: any = {
       status: jest.fn(() => ({
         send: jest.fn((props) => {
           expect(props).toMatchObject({ message: "You lack auth" })
         }),
       })),
-    } // until I figure out how to mock a functinon proper
+    } // until I figure out how to mock a function proper
     await functions.hello(req, res)
     expect(func.logger.warn).toHaveBeenCalledWith("No auth", {
       structuredData: true,

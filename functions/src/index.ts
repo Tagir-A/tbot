419dfc9187e0b1ccce659fa178277a42701bd9ee
Tagir Almasov <tccup1@gmail.com>
2021-10-23 10:57:26 +0000

    change eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions"
 import { Telegraf, Scenes } from "telegraf"
-import fetch from 'node-fetch'
+import fetch from "node-fetch"
 
 import { DONAT_WIZARD_SCENE_ID, donatWizardScene } from "./DonatScene"
 import { superWizard } from "./TestScene"
